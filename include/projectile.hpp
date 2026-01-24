671c11767152ff25ac75b19b1e32e5d3c5d8021c
lPrecisionz <pedrodocarmo1@gmail.com>
2026-01-24 18:38:27 -0300

    add proj start pos

--- include/projectile.hpp
+++ include/projectile.hpp
@@ -5,11 +5,12 @@ namespace Asteroids {
 
 class Projectile : public Entity{
 public: 
-  float m_lifetime {1000.0f};
+  const point start_pos;
+  const float m_max_distance {0.5f};
 
 public:
-Projectile(const point &pos, const point &vel, const std::string &mesh_id, const float &scale, const float &angle) 
-    : Entity(pos, vel, mesh_id, scale, angle){}
+  Projectile(const point &pos, const point &vel, const std::string &mesh_id, const float &scale, const float &angle) 
+    : Entity(pos, vel, mesh_id, scale, angle), start_pos(pos){}
 };
 
 } // namespace Asteroids
