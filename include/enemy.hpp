7bbb6c957fd3895297b9b4f21622afbaa127db06
lPrecisionz <pedrodocarmo1@gmail.com>
2026-01-25 17:56:20 -0300

    compute enemy.hitbox_size

--- include/enemy.hpp
+++ include/enemy.hpp
@@ -14,12 +14,14 @@ class Enemy : public Entity {
 private: 
   constexpr static float uni_lower_bound =-2; 
   constexpr static float uni_upper_bound = 2;
+  constexpr static float baseline_hitbox = 1;
   EnemyState m_state;
-
+  const float hitbox_size;
 public: 
   Enemy(const point &pos, const point &vel, const std::string &mesh_id, const float &scale, const float &angle) 
   : Entity(pos, vel, mesh_id, scale, angle), 
-    m_state(EnemyState::WANDERING){}
+    m_state(EnemyState::WANDERING), 
+    hitbox_size(baseline_hitbox * scale){}
   void update(const float &dt) override;
 
 private:
