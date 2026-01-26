351b8c91e53549414f4a8a9b14bc3d086d421ca0
lPrecisionz <pedrodocarmo1@gmail.com>
2026-01-26 19:28:32 -0300

    add player.handle()

--- include/player.hpp
+++ include/player.hpp
@@ -5,18 +5,21 @@
 namespace Asteroids {
 
 enum PlayerState{
-  SPINNING,
+  SPIN_RIGHT,
+  SPIN_LEFT,
   IDLE
 };
 
 class Player : public Entity{
 public:
+  PlayerState m_state { IDLE };
   Player(const point &pos, const point &vel, const std::string &mesh_id, const float &scale, const float &angle) 
     : Entity(pos, vel, mesh_id, scale, angle){
       m_id     = EntityID::PLAYER;
       m_radius = SHIP_HEIGHT*scale;
   }
   void rotate(const float &angle);
+  void handle();
 };
 
 };
--- src/player.cpp
+++ src/player.cpp
@@ -6,4 +6,17 @@ void Player::rotate(const float &angle){
   m_angle+=angle;
 };
 
+void Player::handle(){
+  switch(m_state){
+    case PlayerState::SPIN_RIGHT:
+      rotate(m_speed);
+      break;
+    case PlayerState::SPIN_LEFT:
+      rotate(-m_speed);
+      break;
+    default:
+      return;
+  }
+}
+
 } // namespace Asteroids
